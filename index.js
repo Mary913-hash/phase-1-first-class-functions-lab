8c3f1a9b348c567d1d11e5bea4e12b1cd7e341e4
mary913 <Patrivimwenge@gmail.com>
2024-09-26 10:41:46 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arrayOfScubaDrivers) => { 
+    const drivers = arrayOfScubaDrivers.slice(0,2)
+    return drivers
+}
+
+
+const returnLastTwoDrivers = (arrayOfLastTwoScubaDrivers) => { 
+    const drivers = arrayOfLastTwoScubaDrivers.slice(-2)
+    return drivers
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+const createFareMultiplier = (multiplier) => { 
+    return (fare) => fare * multiplier
+}
+
+
+const fareDoubler = (fare) => { 
+    const doubledFare = createFareMultiplier(2)
+    return doubledFare(fare)
+}
+
+
+const fareTripler = (fare) => { 
+    const tripledFare = createFareMultiplier(3)
+    return tripledFare(fare)
+}
+
+
+const selectDifferentDrivers = (arrayOfDrivers, slicingFunction) => { 
+    return slicingFunction(arrayOfDrivers)
+}
